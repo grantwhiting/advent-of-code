7acebd6369c4fe9c0c5345ea3f9253f2dc1ac39d
Grant Whiting <grant.whiting@daveramsey.com>
2021-12-11 11:30:10 -0600

    d7c2

---
+++ 2021/Day-7/challenge-2.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const crabPos = fs.readFileSync("./data.txt", "utf-8").split(",").map(Number);
+
+// make object with arrays for difference of each board position and crab position
+const positionArrays = {};
+
+crabPos.forEach((_, i) => {
+  positionArrays[i] = [];
+});
+
+// loop through crab positions and subtract the board position from each crab position
+crabPos.forEach((p1, i) => {
+  crabPos.forEach((p2) => {
+    const difference = Math.abs(p1 - p2);
+
+    positionArrays[i].push((difference * (difference + 1)) / 2);
+  });
+});
+
+// sum up all position arrays in object and get min value
+const summedArrays = Object.values(positionArrays).map((val) =>
+  val.reduce((a, b) => a + b)
+);
+
+console.log(Math.min(...summedArrays));
