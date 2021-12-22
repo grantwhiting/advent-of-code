377226ed2c4b23fdfb2d5a75f439720499d03d01
Grant Whiting <grant.whiting@daveramsey.com>
2021-12-22 08:12:08 -0600

    d11c2

---
+++ 2021/Day-11/challenge-2.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+const matrix = fs
+  .readFileSync("./data.txt", "utf-8")
+  .split(/\n/)
+  .map((x) => [...x])
+  .map((x) => x.map(Number));
+
+let flashes = 0;
+
+for (let i = 0; i < 1000; i++) {
+  powerUp(matrix);
+
+  matrix.forEach((row, i) =>
+    row.forEach((_, j) => {
+      if (matrix[i][j] > 9) {
+        flash(matrix, i, j);
+      }
+    })
+  );
+  reset(matrix);
+
+  if (flashDance(matrix)) {
+    console.log(i + 1);
+    break;
+  }
+}
+
+function flash(matrix, i, j) {
+  flashes++;
+
+  if (matrix[i][j] === "✅") {
+    return;
+  }
+
+  matrix[i][j] = "✅";
+  // get indices of neighbors
+  const neighbors = getNeighborsIndices(i, j);
+  neighbors.forEach(([ni, nj]) => {
+    if (matrix[ni] && matrix[ni][nj]) {
+      if (matrix[ni][nj] !== "✅") {
+        matrix[ni][nj]++;
+      }
+      if (matrix[ni][nj] > 9) {
+        flash(matrix, ni, nj);
+      }
+    }
+  });
+}
+
+function getNeighborsIndices(i, j) {
+  return [
+    [i - 1, j],
+    [i - 1, j + 1],
+    [i, j + 1],
+    [i + 1, j + 1],
+    [i + 1, j],
+    [i + 1, j - 1],
+    [i, j - 1],
+    [i - 1, j - 1],
+  ];
+}
+
+function powerUp(matrix) {
+  matrix.forEach((row, i) =>
+    row.forEach((_, j) => {
+      matrix[i][j]++;
+    })
+  );
+}
+
+function reset(matrix) {
+  matrix.forEach((row, i) =>
+    row.forEach((_, j) => {
+      if (matrix[i][j] === "✅") {
+        matrix[i][j] = 0;
+      }
+    })
+  );
+}
+
+function flashDance(matrix) {
+  return matrix.flatMap((x) => x).every((x) => x === 0);
+}
