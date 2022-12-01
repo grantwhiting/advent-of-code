da24ad938405d65336f65c59a6225b2e5e02309f
Grant Whiting <grant.whiting@daveramsey.com>
2022-12-01 14:04:16 -0600

    ref

--- 2022/Day-1/1.js
+++ 2022/Day-1/1.js
@@ -2,10 +2,6 @@ const fs = require("fs");
 const elfFood = fs
   .readFileSync("./data.txt", "utf-8")
   .split(/\n\s*\n/)
-  .map((x) => x.split(/\n/));
-
-const getTotalCalories = (arr) =>
-  arr.reduce((curr, acc) => (curr += Number(acc)), 0);
-const totalCaloriesArray = elfFood.map((arr) => getTotalCalories(arr));
-const maxCalories = Math.max(...totalCaloriesArray);
-console.log(maxCalories);
+  .map((x) => x.split(/\n/))
+  .map((arr) => arr.reduce((curr, acc) => (curr += Number(acc)), 0));
+console.log(Math.max(...elfFood));
--- 2022/Day-1/2.js
+++ 2022/Day-1/2.js
@@ -2,12 +2,13 @@ const fs = require("fs");
 const elfFood = fs
   .readFileSync("./data.txt", "utf-8")
   .split(/\n\s*\n/)
-  .map((x) => x.split(/\n/));
-
-const reducer = (arr) => arr.reduce((curr, acc) => (curr += Number(acc)), 0);
-const totalCaloriesArrayDesc = elfFood
+  .map((x) => x.split(/\n/))
   .map((arr) => reducer(arr))
-  .sort((a, b) => b - a);
-const top3Total = reducer(totalCaloriesArrayDesc.slice(0, 3));
+  .sort((a, b) => b - a)
+  .slice(0, 3);
 
-console.log(top3Total);
+function reducer(arr) {
+  return arr.reduce((curr, acc) => (curr += Number(acc)), 0);
+}
+
+console.log(reducer(elfFood));
