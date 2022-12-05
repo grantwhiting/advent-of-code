a0b0b0c9b4f25232c1908fd53530da694c3e98c7
Grant Whiting <grant.whiting@daveramsey.com>
2022-12-05 11:41:34 -0600

    .

---
+++ 2022/Day-5/2.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const stacks = fs
+  .readFileSync("./data_input.txt", "utf-8")
+  .split(/\n/)
+  .map((x) => x.split(","));
+const instructions = fs
+  .readFileSync("./data_instructions.txt", "utf-8")
+  .split(/\n/)
+  .map((x) => x.split(" "))
+  .map((x) => ({
+    move: Number(x[1]),
+    from: Number(x[3]) - 1,
+    to: Number(x[5]) - 1,
+  }));
+
+const messageArr = [];
+
+instructions.forEach((ins, idx) => {
+  const { move, from, to } = ins;
+
+  const itemsToMove = [...new Array(move)].flatMap((_) => stacks[from].pop());
+
+  itemsToMove.reverse().forEach((item) => {
+    stacks[to].push(item);
+  });
+});
+
+stacks.forEach((s) => {
+  messageArr.push(s[s.length - 1]);
+});
+
+console.log(messageArr.join(""));
