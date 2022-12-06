96de2a2f18f900badce2ba93d464b5c47e434b89
Grant Whiting <grant.whiting@daveramsey.com>
2022-12-06 08:18:34 -0600

    .

---
+++ 2022/Day-6/2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const chars = fs.readFileSync("./data.txt", "utf-8");
+
+for (let i = 0; i < chars.length; i++) {
+  const count = 14;
+  const uniqueChars = new Set();
+  const chunk = chars.slice(i, i + count);
+
+  chunk.split("").forEach((x) => uniqueChars.add(x));
+
+  if (uniqueChars.size === count) {
+    console.log(i + count);
+    break;
+  }
+}
