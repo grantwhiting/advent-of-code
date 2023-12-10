8990d5e017354689d90befb40fbcddd92cf417b8
grant.whiting <whiting.grant@gmail.com>
2023-12-09 22:32:10 -0600

    more day 3 crap

--- 2023/3/1.js
+++ 2023/3/1.js
@@ -1,14 +1,52 @@
 const fs = require("fs");
-const schematic = fs.readFileSync("./sample.txt", "utf-8").split(/\n/);
+const schematic = fs
+  .readFileSync("./sample.txt", "utf-8")
+  .trim()
+  .split("\n")
+  .map((row) => row.split(""));
 
-const numbersAdjacentToSymbols = [];
+const numbers = [];
 const symbols = [];
 
 const isSymbol = (str) =>
   str !== undefined ? (str.match(/[^0-9.]+/g) ? true : false) : false;
 const isNumber = (str) =>
   str !== undefined ? (str.match(/\d+/g) ? true : false) : null;
+const numbersNotPrecededOrFollowedBySymbol = (str) =>
+  str.match(/(?<![#$*+])\b\d+\b(?![#$*+])/g);
 
-const numbersNotPrecededOrFollowedBySymbol = schematic.map((row) =>
-  row.match(/(?<![#$*+])\b\d+\b(?![#$*+])/g)
-);
+schematic.forEach((row, y) => {
+  row.forEach((item, x) => {
+    if (isSymbol(item)) {
+      symbols.push({
+        symbol: item,
+        x: x,
+        y: y,
+        surroundingCells: checkAdjacentCells(item, x, y),
+      });
+    }
+
+    if (isNumber(item)) {
+      numbers.push({ symbol: item, coordinates: { x, y } });
+    }
+  });
+});
+
+function checkAdjacentCells(x, y) {
+  return [
+    { x: x, y: y - 1 },
+    { x: x + 1, y: y - 1 },
+    { x: x + 1, y: y },
+    { x: x + 1, y: y + 1 },
+    { x: x, y: y + 1 },
+    { x: x - 1, y: y + 1 },
+    { x: x - 1, y: y },
+    { x: x - 1, y: y - 1 },
+  ];
+}
+
+const symbolAdjacentCellCoordinates = symbols.map((s) => s.surroundingCells);
+
+numbers.forEach((number) => {});
+
+console.dir(numbers, { depth: null });
