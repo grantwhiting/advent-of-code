c1daa8948cbbed6eae3f39aee8b9cef585be4670
grant.whiting <whiting.grant@gmail.com>
2023-12-10 22:29:35 -0600

    d3p1

--- 2023/3/1.js
+++ 2023/3/1.js
@@ -1,52 +1,70 @@
 const fs = require("fs");
 const schematic = fs
-  .readFileSync("./sample.txt", "utf-8")
+  .readFileSync("./data.txt", "utf-8")
   .trim()
   .split("\n")
   .map((row) => row.split(""));
-
-const numbers = [];
-const symbols = [];
 
 const isSymbol = (str) =>
   str !== undefined ? (str.match(/[^0-9.]+/g) ? true : false) : false;
 const isNumber = (str) =>
   str !== undefined ? (str.match(/\d+/g) ? true : false) : null;
-const numbersNotPrecededOrFollowedBySymbol = (str) =>
-  str.match(/(?<![#$*+])\b\d+\b(?![#$*+])/g);
+const isPeriod = (str) =>
+  str !== undefined ? (str.match(/./g) ? true : false) : null;
+const getAdjacentCells = (x, y) => {
+  return {
+    left: schematic[y][x - 1],
+    bottomLeft: schematic[y + 1] ? schematic[y + 1][x - 1] : undefined,
+    bottom: schematic[y + 1] ? schematic[y + 1][x] : undefined,
+    bottomRight: schematic[y + 1] ? schematic[y + 1][x + 1] : undefined,
+    right: schematic[y][x + 1],
+    topRight: schematic[y - 1] ? schematic[y - 1][x + 1] : undefined,
+    top: schematic[y - 1] ? schematic[y - 1][x] : undefined,
+    topLeft: schematic[y - 1] ? schematic[y - 1][x - 1] : undefined,
+  };
+};
+
+const allNumbers = [];
 
 schematic.forEach((row, y) => {
-  row.forEach((item, x) => {
-    if (isSymbol(item)) {
-      symbols.push({
-        symbol: item,
-        x: x,
-        y: y,
-        surroundingCells: checkAdjacentCells(item, x, y),
+  let builtNumber = "";
+  let coordinates = [];
+  for (let x = 0; x < row.length; x++) {
+    if (isNumber(row[x])) {
+      if (builtNumber === "") builtNumber = row[x];
+      const adjacentCells = getAdjacentCells(x, y);
+      coordinates.push({ y, x });
+      // check if right isNumber
+      if (isNumber(adjacentCells.right)) {
+        builtNumber += adjacentCells.right;
+        continue;
+      }
+      allNumbers.push({
+        number: builtNumber,
+        coordinates,
+        isPartNumber: false,
       });
+      coordinates = [];
+      builtNumber = "";
     }
+  }
+});
 
-    if (isNumber(item)) {
-      numbers.push({ symbol: item, coordinates: { x, y } });
-    }
+allNumbers.forEach((n) => {
+  n.coordinates.forEach((c) => {
+    const adjCells = getAdjacentCells(c.x, c.y);
+    Object.values(adjCells).forEach((ac) => {
+      if (isSymbol(ac)) {
+        n.isPartNumber = true;
+      }
+    });
   });
 });
 
-function checkAdjacentCells(x, y) {
-  return [
-    { x: x, y: y - 1 },
-    { x: x + 1, y: y - 1 },
-    { x: x + 1, y: y },
-    { x: x + 1, y: y + 1 },
-    { x: x, y: y + 1 },
-    { x: x - 1, y: y + 1 },
-    { x: x - 1, y: y },
-    { x: x - 1, y: y - 1 },
-  ];
-}
+const partNumbers = allNumbers
+  .map((n) => {
+    if (n.isPartNumber) return Number(n.number);
+  })
+  .filter((n) => n !== undefined);
 
-const symbolAdjacentCellCoordinates = symbols.map((s) => s.surroundingCells);
-
-numbers.forEach((number) => {});
-
-console.dir(numbers, { depth: null });
+console.log(partNumbers.reduce((acc, cur) => acc + cur));
